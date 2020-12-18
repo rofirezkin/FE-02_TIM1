6b504f7f2f1dfcb70363bd9455de0e9f7d3a6b5a
rezkinrofi <rofirezkin@gmail.com>
2020-12-18 23:53:45 +0700

    koneksi database

--- src/services/getProduct.js
+++ src/services/getProduct.js
@@ -4,5 +4,8 @@ import API from '../config/rest';
 const getProduct = (namaProduct) => {
   return BaseService.get(API.PRODUCT, { namaProduct });
 };
+const getProductId = (productId) => {
+  return BaseService.get(API.PRODUCTBYID(productId));
+};
 
-export default { getProduct };
+export default { getProduct, getProductId };
